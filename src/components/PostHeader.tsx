02da6e63e5a13889ef71354a5c18be10ed7a27c8
ptrlawless <59983278+ptrlawless@users.noreply.github.com>
2021-09-07 13:33:39 -0700

    fixed postHeader props issue and build test

--- src/components/PostHeader.tsx
+++ src/components/PostHeader.tsx
@@ -3,7 +3,7 @@ import DateFormatter from './DateFormatter';
 import CoverImage from './CoverImage';
 import PostTitle from './PostTitle';
 
-export default function PostHeader({ title, coverImage, date, author }) {
+export default function PostHeader({ title, coverImage, slug, date, author }) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
@@ -11,7 +11,13 @@ export default function PostHeader({ title, coverImage, date, author }) {
         <Avatar name={author.name} picture={author.picture} />
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} height={620} width={1240} />
+        <CoverImage
+          title={title}
+          src={coverImage}
+          slug={slug}
+          height={620}
+          width={1240}
+        />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
